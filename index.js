ed657dcbba6bd1a6d431154604b32935667cb478
Olivier BOUDJEMA <olivier.boudjema@thalesdigital.io>
2022-07-18 00:45:02 +0200

    refacto db

--- index.js
+++ index.js
@@ -17,208 +17,87 @@ const binance = new Binance().options({
   APISECRET: process.env.APISECRET,
 });
 
-const db = mysql.createConnection({
-  host: "sql11.freesqldatabase.com",
-  user: "sql11501919",
-  password: "Nju7PD33bb",
-  database: "sql11501919",
-});
-
-db.removeAllListeners();
-
-db.connect(function (err) {
-  if (err) throw err;
-  console.log("Connecté à la base de données MySQL!");
-});
-
-db.promise = (sql) => {
-  console.log("db.promise");
-  return new Promise((resolve, reject) => {
-    db.query(sql, (err, result) => {
-      if (err) {
-        console.log("Error");
-        reject(new Error());
-      } else {
-        console.log("No error");
-        resolve(result);
-      }
-    });
-  }).catch(() => {
-    console.log("Promise errored");
+function db_connection() {
+  const db = mysql.createConnection({
+    host: "sql11.freesqldatabase.com",
+    user: "sql11506948",
+    password: "DavwFu9DjL",
+    database: "sql11506948",
   });
-};
 
-async function connection(query) {
+  db.removeAllListeners();
+
+  db.connect(function (err) {
+    if (err) throw err;
+    console.log("Connecté à la base de données MySQL!");
+  });
+
+  db.promise = (sql) => {
+    return new Promise((resolve, reject) => {
+      db.query(sql, (err, result) => {
+        if (err) {
+          reject(new Error());
+        } else {
+          resolve(result);
+        }
+      });
+    }).catch(() => {
+      console.log("Promise errored");
+    });
+  };
+  return db;
+}
+
+function db_deconnction(db) {
+  db.removeAllListeners();
+  db.end();
+}
+
+async function connection(query, db) {
   const result = await db.promise(query);
   return result;
 }
 
 app.listen(process.env.PORT || port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
 
 app.get("/", async (req, res) => {
   val_bitcoin = await valeur_actuelle_bictoin();
   val_bitcoin = val_bitcoin.BTCUSDT;
-  // console.log("bitcoin_price : " + val_bitcoin)
+  console.log("bitcoin_price : " + val_bitcoin);
   res.setHeader("Content-Type", "application/json");
   res.end("Pedro Oliv trading bot: " + JSON.stringify(val_bitcoin, null, 2));
 });
 
-app.get("/mes_bitcoins", async (req, res) => {
-  mes_bitcoins = await connection(
-    "SELECT * FROM `mes_bitcoins` ORDER BY `mes_bitcoins`.`date` DESC LIMIT 24"
+app.get("/mon_argent_url", async (req, res) => {
+  const db = db_connection();
+  mon_argent = await connection(
+    "SELECT * FROM `mon_argent` ORDER BY `mon_argent`.`date` DESC LIMIT 24",
+    db
   );
-  mes_bitcoins = Object.values(JSON.parse(JSON.stringify(mes_bitcoins)));
-  // console.log(mes_bitcoins)
+  db_deconnction(db);
+  mon_argent = Object.values(JSON.parse(JSON.stringify(mon_argent)));
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(mes_bitcoins));
-  return mes_bitcoins;
+  res.end(JSON.stringify(mon_argent));
+  return mon_argent;
 });
 
-app.get("/mes_euros", async (req, res) => {
-  mes_euros = await connection(
-    "SELECT * FROM `mes_euros` ORDER BY `mes_euros`.`date` DESC LIMIT 24"
-  );
-  mes_euros = Object.values(JSON.parse(JSON.stringify(mes_euros)));
-  // console.log(mes_euros)
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(mes_euros));
-  return mes_euros;
-});
-
-app.get("/bitcoins_cours", async (req, res) => {
-  bitcoins_cours = await connection(
-    "SELECT * FROM `bitcoins_cours` ORDER BY `bitcoins_cours`.`date` DESC LIMIT 24"
-  );
-  bitcoins_cours = Object.values(JSON.parse(JSON.stringify(bitcoins_cours)));
-
-  // console.log(bitcoins_cours)
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(bitcoins_cours));
-  return bitcoins_cours;
-});
-
-app.post("/mes_bitcoins", jsonParser, async function (req, res) {
-  console.log("Endpoint mes bitcoins");
-  quantite_bitcoins_demande_a_vendre = req.body.quantite;
-  mes_bitcoins = await connection(
-    "SELECT * FROM `mes_bitcoins` ORDER BY `mes_bitcoins`.`date` DESC LIMIT 1"
-  );
-  mes_bitcoins = Object.values(JSON.parse(JSON.stringify(mes_bitcoins)));
-  nouvelle_quantite_bitcoins =
-    mes_bitcoins[0].quantite - quantite_bitcoins_demande_a_vendre;
-  console.log(
-    "🚀 ~ file: index.js ~ line 78 ~ nouvelle_quantite_bitcoins",
-    nouvelle_quantite_bitcoins
-  );
-
-  bitcoins_cours = await connection(
-    "SELECT * FROM `bitcoins_cours` ORDER BY `bitcoins_cours`.`date` DESC LIMIT 1"
-  );
-  bitcoins_cours = Object.values(JSON.parse(JSON.stringify(bitcoins_cours)));
-  bitcoins_cours = bitcoins_cours[0].valeur;
-  console.log("🚀 ~ file: index.js ~ line 83 ~ bitcoins_cours", bitcoins_cours);
-
-  ancienn_quantite_euro = await connection(
-    "SELECT * FROM `mes_euros` ORDER BY `mes_euros`.`date` DESC LIMIT 1"
-  );
-  ancienn_quantite_euro = Object.values(
-    JSON.parse(JSON.stringify(ancienn_quantite_euro))
-  )[0].quantite;
-  nouvelle_quantite_euros =
-    ancienn_quantite_euro +
-    convert_bitcoins_to_euros(
-      quantite_bitcoins_demande_a_vendre,
-      bitcoins_cours
-    );
-  console.log(
-    "🚀 ~ file: index.js ~ line 86 ~ nouvelle_quantite_euros",
-    nouvelle_quantite_euros
-  );
-
-  console.log("🚀 ~ file: index.js ~ line 88 ~ req.body.date", req.body.date);
+app.post("/mon_argent_url", jsonParser, async function (req, res) {
+  const db = db_connection();
   db.query(
-    "INSERT INTO `mes_euros` (`id`, `monnaie`, `quantite`, `date`) VALUES (NULL, '" +
-      "euros" +
+    "INSERT INTO `mon_argent` (`id`, `mes_euros`, `mes_bitcoins`, `bitcoin_cours`, `date`) VALUES (NULL, '" +
+      req.body.mes_euros +
       "', '" +
-      nouvelle_quantite_euros +
+      req.body.mes_bitcoins +
+      "', '" +
+      req.body.bitcoin_cours +
       "', '" +
       req.body.date +
       "')"
   );
-  db.query(
-    "INSERT INTO `mes_bitcoins` (`id`, `monnaie`, `quantite`, `date`) VALUES (NULL, '" +
-      "bitcoins" +
-      "', '" +
-      nouvelle_quantite_bitcoins +
-      "', '" +
-      req.body.date +
-      "')"
-  );
-  console.log("Base de donnée mise à jour avec succès !");
-  res.sendStatus(200);
-});
 
-app.post("/mes_euros", jsonParser, async function (req, res) {
-  console.log("Endpoint mes euros");
-  quantite_euros_demande_a_vendre = req.body.quantite;
-  mes_euros = await connection(
-    "SELECT * FROM `mes_euros` ORDER BY `mes_euros`.`date` DESC LIMIT 1"
-  );
-  mes_euros = Object.values(JSON.parse(JSON.stringify(mes_euros)));
-  nouvelle_quantite_euros =
-    mes_euros[0].quantite - quantite_euros_demande_a_vendre;
-  console.log(
-    "🚀 ~ file: index.js ~ line 96 ~ nouvelle_quantite_euros",
-    nouvelle_quantite_euros
-  );
-
-  bitcoins_cours = await connection(
-    "SELECT * FROM `bitcoins_cours` ORDER BY `bitcoins_cours`.`date` DESC LIMIT 1"
-  );
-  bitcoins_cours = Object.values(JSON.parse(JSON.stringify(bitcoins_cours)));
-  bitcoins_cours = bitcoins_cours[0].valeur;
-  console.log(
-    "🚀 ~ file: index.js ~ line 101 ~ bitcoins_cours",
-    bitcoins_cours
-  );
-
-  ancienne_qantite_bitcoins = await connection(
-    "SELECT * FROM `mes_bitcoins` ORDER BY `mes_bitcoins`.`date` DESC LIMIT 1"
-  );
-  ancienne_qantite_bitcoins = Object.values(
-    JSON.parse(JSON.stringify(ancienne_qantite_bitcoins))
-  )[0].quantite;
-
-  nouvelle_quantite_bitcoins =
-    ancienne_qantite_bitcoins +
-    convert_euros_to_bitcoins(quantite_euros_demande_a_vendre, bitcoins_cours);
-  console.log(
-    "🚀 ~ file: index.js ~ line 103 ~ nouvelle_quantite_bitcoins",
-    nouvelle_quantite_bitcoins
-  );
-
-  console.log("🚀 ~ file: index.js ~ line 106 ~ req.body.date", req.body.date);
-  db.query(
-    "INSERT INTO `mes_euros` (`id`, `monnaie`, `quantite`, `date`) VALUES (NULL, '" +
-      "euros" +
-      "', '" +
-      nouvelle_quantite_euros +
-      "', '" +
-      req.body.date +
-      "')"
-  );
-  db.query(
-    "INSERT INTO `mes_bitcoins` (`id`, `monnaie`, `quantite`, `date`) VALUES (NULL, '" +
-      "bitcoins" +
-      "', '" +
-      nouvelle_quantite_bitcoins +
-      "', '" +
-      req.body.date +
-      "')"
-  );
-  console.log("Base de donnée mise à jour avec succès !");
+  console.log(req.body.date, " Base de donnée mise à jour avec succès !");
   res.sendStatus(200);
 });
 
@@ -227,45 +106,31 @@ async function valeur_actuelle_bictoin() {
   return fprices;
 }
 
-function save_mes_euros_to_db(monnaie, quantite, date) {
+function save_mon_argent_to_db(mes_euros, mes_bitcoins, bitcoin_cours, date) {
+  const db = db_connection();
   db.query(
-    "INSERT INTO `mes_euros` (`id`, `monnaie`, `quantite`, `date`) VALUES (NULL, '" +
-      monnaie +
+    "INSERT INTO `mon_argent` (`id`, `mes_euros`, `mes_bitcoins`, `bitcoin_cours`, `date`) VALUES (NULL, '" +
+      mes_euros +
       "', '" +
-      quantite +
+      mes_bitcoins +
+      "', '" +
+      bitcoin_cours +
       "', '" +
       date +
       "')"
   );
+  db_deconnction(db);
   console.log(
-    "Mes euros : " + monnaie + " " + quantite + " " + date + " inséré!"
-  );
-}
-
-function save_mes_bitcoins_to_db(monnaie, quantite, date) {
-  db.query(
-    "INSERT INTO `mes_bitcoins` (`id`, `monnaie`, `quantite`, `date`) VALUES (NULL, '" +
-      monnaie +
-      "', '" +
-      quantite +
-      "', '" +
+    "Mon argent : " +
+      mes_euros +
+      " " +
+      mes_bitcoins +
+      " " +
+      bitcoin_cours +
+      " " +
       date +
-      "')"
+      " insérés !"
   );
-  console.log(
-    "Mes bitcoins : " + monnaie + " " + quantite + " " + date + " inséré!"
-  );
-}
-
-function save_bitcoin_cours_to_db(valeur, date) {
-  db.query(
-    "INSERT INTO `bitcoins_cours` (`valeur`, `date`) VALUES ('" +
-      valeur +
-      "',  '" +
-      date +
-      "')"
-  );
-  console.log("Cours du bitcoin : " + valeur + " " + date + " inséré!");
 }
 
 const save_all_in_db = async function () {
@@ -276,53 +141,35 @@ const save_all_in_db = async function () {
     .slice(0, 19)
     .replace("T", " ");
 
-  console.log("Oui bonsoir execute à : " + date_now);
   const res = await valeur_actuelle_bictoin();
   valeur_bitcoin = res.BTCUSDT;
-
-  mes_euros = await connection(
-    "SELECT * FROM `mes_euros` ORDER BY `mes_euros`.`date` DESC LIMIT 1"
+  const db = db_connection();
+  mon_argent = await connection(
+    "SELECT * FROM `mon_argent` ORDER BY `mon_argent`.`date` DESC LIMIT 1",
+    db
   );
-  mes_euros = Object.values(JSON.parse(JSON.stringify(mes_euros)));
+  mon_argent = Object.values(JSON.parse(JSON.stringify(mon_argent)));
 
-  mes_bitcoins = await connection(
-    "SELECT * FROM `mes_bitcoins` ORDER BY `mes_bitcoins`.`date` DESC LIMIT 1"
+  db_deconnction(db);
+  save_mon_argent_to_db(
+    mon_argent[0].mes_euros,
+    mon_argent[0].mes_bitcoins,
+    valeur_bitcoin,
+    date_now
   );
-  mes_bitcoins = Object.values(JSON.parse(JSON.stringify(mes_bitcoins)));
-
-  save_mes_euros_to_db("euros", mes_euros[0].quantite, date_now);
-  save_mes_bitcoins_to_db("bitcoins", mes_bitcoins[0].quantite, date_now);
-  save_bitcoin_cours_to_db(valeur_bitcoin, date_now);
 };
 
 async function runFunction() {
   const t = setInterval(async function () {
     if (new Date().getMinutes() % 15 === 0) {
-      const db = mysql.createConnection({
-        host: "sql11.freesqldatabase.com",
-        user: "sql11501919",
-        password: "Nju7PD33bb",
-        database: "sql11501919",
-      });
-
-      db.connect(function (err) {
-        if (err) throw err;
-        console.log("Connecté à la base de données MySQL!");
-      });
-      console.log("efin quart heure" + new Date().getMinutes());
+      const db = db_connection();
+      console.log(new Date().getMinutes());
       await save_all_in_db();
+      db_deconnction(db);
     } else {
-      console.log("pas quart heure : " + new Date().getMinutes());
+      console.log(new Date().getMinutes());
     }
   }, 60000);
 }
 
 runFunction();
-
-function convert_euros_to_bitcoins(quantite_euros, bitcoins_cours) {
-  return quantite_euros / bitcoins_cours;
-}
-
-function convert_bitcoins_to_euros(quantite_bitcoin, bitcoins_cours) {
-  return bitcoins_cours * quantite_bitcoin;
-}
